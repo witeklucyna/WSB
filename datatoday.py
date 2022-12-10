e7121e4eb3ce649dde14e44b5be98049eb9eae0f
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 13:13:35 +0100

    dataupdate

--- datatoday.py
+++ datatoday.py
@@ -3,3 +3,8 @@ import datetime
 today = datetime.date.today()
 print(type(today))
 print(today)
+
+data1 = today.strftime("Dzisiaj jest %d dzien ..... %m miesiaca")
+data2 = today.strftime('%d--%m--%y')
+print(data1)
+print(data2)
