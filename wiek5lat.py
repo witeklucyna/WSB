3be8d90149cbd590a225059b3de4545d58ab5e74
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 13:39:52 +0100

    wiek5lat

---
+++ wiek5lat.py
@@ -0,0 +1,13 @@
+#program otrzymuje liczbe 4ro cyfrowa
+#zwraca sume cyfr
+
+liczba = (input('podaj liczbe'))
+print(liczba[0])
+print(liczba[1])
+print(liczba[2])
+print(liczba[3])
+
+suma = (int(liczba[0]) + int(liczba[1]) + int(liczba[2]) + int(liczba[3]))
+print(suma)
+
+#jak zabezpieczyc przez 2 cyfrowa / znakami innymi ?
