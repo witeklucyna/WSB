e9c43daa4b4b73d1deb72a5d031418cefbe9db2d
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-10 14:54:22 +0100

    update zadanie csv

--- zadanie_csv.py
+++ zadanie_csv.py
@@ -9,3 +9,20 @@ for i in range (len(content)):
     content[i] = content[i].split(',')
 print(content)
 print(content[1][1])
+
+
+#rozliczanie sredniej wyplaty
+
+total = 0
+for i in range (1,len(content)): #zeby nie leciec od 0 - dajemy 1
+    total += int(content[i][1]) # zamiast += mozna uzyc total +
+average = total / (len(content) -1)
+print(round(average,2))
+
+#ile kobiet na miecierzynskim
+
+total = 0
+for i in range(1,len(content)):
+    if content[i][3] == 'k' and content[i][4] == 't':
+        total += 1
+print(total)
