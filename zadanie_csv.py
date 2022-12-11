c9d7b954c6aab1e3c1276a75e49918563900b6e9
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-12-11 09:30:59 +0100

    update zadania

--- zadanie_csv.py
+++ zadanie_csv.py
@@ -6,7 +6,7 @@ print(content[3])
 
 
 for i in range (len(content)):
-    print(content[i])
+    #print(content[i])
     content[i] = content[i].split(',') #wez kolejna linie, )tyle linii ile len) , czyli content[0],[1] itp i podziel ja
     print(content[i])
 
@@ -14,11 +14,21 @@ for i in range (len(content)):
 #rozliczanie sredniej wyplaty
 
 print('\nRozliczenie średniej wypłaty')
-total = 0
-for i in range (1,len(content)): #zeby nie leciec od 0 - dajemy 1
-    total += int(content[i][1]) # zamiast += mozna uzyc total +
-average = total / (len(content) -1)
+total = 0 #wartosc pocztkowa, potem po len bedziemy do niej dodawac +1
+for i in range (1, len(content)): #zeby nie leciec od 0 - dajemy 1
+    total += int(content[i][1]) # zamiast += mozna uzyc total + content[i][1] bo wynagrodzenie to druga kolumna|| z defaultu to co mamy w pliku python widzi jako string, stad int, ale moze warto float?
+average = total / (len(content)-1)
 print(round(average,2))
+
+#inny przyklad
+
+# print('\nInny przyklad')
+# for i in range(15):
+#     print(i)
+#
+# print('\nJeszcze inny przyklad') # przeanalizowac czemu printuje dziwne rzeczy
+# for i in range(15, 3 ,-2):
+#     print(i)
 
 #ile kobiet na miecierzynskim
 
